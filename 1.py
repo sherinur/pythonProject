39ed678e6182e59ac864408ebe3119b9e42fc0e1
Sheri Nurislam <sherinurislam@gmail.com>
2024-02-02 21:47:16 +0600

    second action

--- 1.py
+++ 1.py
@@ -1,6 +1,37 @@
-file = open('text.txt', 'w')
+class Pet():
+    def __init__(self, name, animal_type, age):
+        """ constructor """
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
 
-file.write('Encapsulation: Wrapping data and methods into a single unit (class).')
+    """ getter method """
+    def get_name(self):
+        return self.__name
+
+    def get_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
+
+    """ setter method """
+    def set_name(self, name):
+        self.__name = name
+
+    def set_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
 
 
-file.close()
+name = input("What is name of your pet? ")
+type = input("What type of your pet? ")
+age = input("How old is your pet? ")
+
+pet1 = Pet(name, type, age)
+
+print(pet1.get_name())
+print(pet1.get_type())
+print(pet1.get_age())
